24fafda6838a0aad1583dc65991c61c7b2737e88
David Gaspar <davidgaspar.dev@gmail.com>
2022-06-28 22:44:44 -0300

    Example code added

---
+++ examples/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"golog"
+)
+
+func main() {
+	golog.Info("Intelmes", "Intelmes is a manufacturing executation (MES) system to Intelbras")
+	golog.Service("Intelmes", "Machines modbus started")
+	golog.System("Intelmes", "Routes created")
+	golog.Error("Intelmes", fmt.Errorf("Failed to generate OEE"))
+	golog.Warn("Intelmes", "Unable to add resource")
+}
