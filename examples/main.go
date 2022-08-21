731d7774e359b895bd62e7bbef54e9fbe7dac522
David Gaspar <davidgaspar.dev@gmail.com>
2022-08-21 20:38:07 -0300

    chore: update example code to new feature

--- examples/main.go
+++ examples/main.go
@@ -12,4 +12,21 @@ func main() {
 	golog.System("Intelmes", "Routes created")
 	golog.Error("Intelmes", fmt.Errorf("Failed to generate OEE"))
 	golog.Warn("Intelmes", "Unable to add resource")
+
+	log := golog.NewLogTag("Database")
+	log.Info("Create", "MES_resources table")
+	log.Service("Clean", "remove last data in MES_resources")
+	log.System("Run", "start setup script")
+	log.Error("GetItems", fmt.Errorf("columns not exists"))
+	log.Warn("GetUserById", "id is only number")
+
+	fmt.Println("\n------ Email Service Exmaple (README) ------")
+	SendEmail("Hello World")
+}
+
+var log = golog.NewLogTag("Email Service")
+
+func SendEmail(message string) {
+	log.Service("SendEmail", "sending email with this content: "+message)
+	// Code here
 }
